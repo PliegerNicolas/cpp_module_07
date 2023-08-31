921235599f11f85c62801a5d9627860c2e33e38c
Nicolas PLIEGER <plieger05@gmail.com>
2023-08-31 14:11:52 +0200

    Corrected output problem

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nplieger <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 13:26:09 by nplieger          #+#    #+#             */
-/*   Updated: 2023/08/31 13:56:20 by nplieger         ###   ########.fr       */
+/*   Updated: 2023/08/31 14:11:34 by nplieger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "iter.hpp"
@@ -19,6 +19,7 @@
 #define GRAY "\033[037;2m"
 #define CLEAR "\033[0m"
 
+/* start custom class */
 class Awesome
 {
 	public:
@@ -51,7 +52,14 @@ std::ostream	&operator<<(std::ostream &os, const Awesome &awesome)
 	os << awesome.getN();
 	return (os);
 }
+/* end custom class */
 
+/* start test functions */
+static void	increment(int &nbr)
+{
+	nbr++;
+}
+/* end test functions */
 
 static void	testInt(void)
 {
@@ -63,8 +71,13 @@ static void	testInt(void)
 	for (int i = 0; i < SIZE; i++)
 		arr[i] = i;
 
+	std::cout << GRAY << "Call ::iter(arr, SIZE, &increment)" << CLEAR << std::endl;
+	::iter(arr, SIZE, &increment);
+	std::cout << std::endl;
+
 	std::cout << GRAY << "Call ::iter(arr, SIZE, &print)" << CLEAR << std::endl;
 	::iter(arr, SIZE, &print);
+	std::cout << std::endl;
 }
 
 static void	testDouble(void)
@@ -79,6 +92,7 @@ static void	testDouble(void)
 
 	std::cout << GRAY << "Call ::iter(arr, SIZE, &print)" << CLEAR << std::endl;
 	::iter(arr, SIZE, &print);
+	std::cout << std::endl;
 }
 
 static void	testString(void)
@@ -93,6 +107,7 @@ static void	testString(void)
 
 	std::cout << GRAY << "Call ::iter(arr, SIZE, &print)" << CLEAR << std::endl;
 	::iter(arr, SIZE, &print);
+	std::cout << std::endl;
 }
 
 static void	testClass(void)
@@ -107,7 +122,7 @@ static void	testClass(void)
 
 	std::cout << GRAY << "Call ::iter(arr, SIZE, &print)" << CLEAR << std::endl;
 	::iter(arr, SIZE, &print);
-
+	std::cout << std::endl;
 }
 
 
